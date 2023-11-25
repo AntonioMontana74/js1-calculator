76c2f47d8433ea85635c8ef4bc23d6895f56989c
AntonioMontana74 <149238712+AntonioMontana74@users.noreply.github.com>
2023-11-25 08:44:24 +0300

    Add files via upload

---
+++ calculator/main.js
@@ -0,0 +1,35 @@
+const OPERATIONS = {
+    sum: '+',
+    substract: '-',
+    multiply: '*',
+    division: '/',
+}
+
+function calculate({a, b, operation}) {
+    let result = null;
+
+    switch (operation) {
+        case OPERATIONS.sum:
+            result = sum(a, b);
+            break;
+
+        case OPERATIONS.substract:
+            result = substract(a, b);
+            break;
+
+        case OPERATIONS.multiply:
+            result = multiply(a, b);
+            break;
+
+
+        case OPERATIONS.division:
+            result = division(a, b);
+            break;
+
+    
+        default:
+            return "Неверный оператор."
+    }
+
+    return result;
+}
